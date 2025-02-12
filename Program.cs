923beef7c2f7249013f2c9c5481b755fbc8dc7e8
elorasmith <eloraasmith@gmail.com>
2025-02-12 15:51:28 -0700

    Final commit

--- Program.cs
+++ Program.cs
@@ -2,15 +2,11 @@
 
 // Elora Smith, 2/12/25, Lab 3 Mastermind
 
-using System.Collections.Concurrent;
-using System.Runtime.Intrinsics.Arm;
-
 Console.WriteLine("Welcome!");
 Console.WriteLine("I have chosen 4 letters between \"a\" and \"g\" and arranged them in a particular order. \nYour job is to guess the letters and put them in the right order.");
 string secret = "aefb";
 string? guess = "aaaa";
 int guessNum = 1;
-
 
 do
 {
