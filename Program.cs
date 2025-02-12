46ea367b0afa25c2236cd7242821bfa79d958dc1
elorasmith <eloraasmith@gmail.com>
2025-02-12 14:41:48 -0700

    2nd commit

--- Program.cs
+++ Program.cs
@@ -10,18 +10,24 @@ Console.WriteLine("I have chosen 4 letters between \"a\" and \"g\" and arranged 
 string secret = "aefb";
 string guess = "aaaa";
 int guessNum = 1;
-int correctPositions = 0;
 
 
 do
 {
+    int correctPositions = 0;
+    int correctLetters = 0;
     Console.WriteLine($"Guess {guessNum}: Please guess a sequence of 4 lowercase letters with no repeats.");
     guess = Console.ReadLine();
     guessNum ++;
     for (int i = 0; i < secret.Length; i++)
         if (guess[i] == secret[i])
             correctPositions++;
+    for (int i = 0; i < secret.Length; i++)
+        if (guess.Contains(secret[i]))
+            correctLetters++;
+    correctLetters -= correctPositions;
     Console.WriteLine($"-- {correctPositions} letters in the correct position.");
+    Console.WriteLine($"-- {correctLetters} correct letters in the wrong position.");
 }
 while (secret != guess);
-Console.WriteLine(guess);
+Console.WriteLine($"Congrats! You guessed the secret: {secret}. It took you {guessNum} guesses!");
